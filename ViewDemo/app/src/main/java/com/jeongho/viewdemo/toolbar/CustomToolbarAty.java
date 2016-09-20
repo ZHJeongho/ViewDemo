746d4df9e7a9f35d5d37b34162422baf71e1855c
JeonghoMac <zhfighting2014@163.com>
2016-09-20 23:11:51 +0800

    add customToolbar

--- ViewDemo/app/src/main/java/com/jeongho/viewdemo/toolbar/CustomToolbarAty.java
+++ ViewDemo/app/src/main/java/com/jeongho/viewdemo/toolbar/CustomToolbarAty.java
@@ -1,7 +1,46 @@
 package com.jeongho.viewdemo.toolbar;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+
+import com.jeongho.viewdemo.BaseActivity;
+import com.jeongho.viewdemo.R;
+
 /**
  * Created by Jeongho on 2016/9/9.
  */
-public class CustomToolbarAty {
+public class CustomToolbarAty extends BaseActivity{
+    private String TAG = getClass().getName();
+    private CustomToolbar mCustomToolbar;
+    @Override
+    public void initView() {
+        mCustomToolbar = (CustomToolbar) findViewById(R.id.ct);
+    }
+
+    @Override
+    public void initData() {
+        mCustomToolbar.setTitle(R.string.home);
+    }
+
+    @Override
+    public void initListener() {
+        mCustomToolbar.setRightOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "right clicked");
+            }
+        });
+    }
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_custom_toolbar;
+    }
+
+    public static void startAction(Context context) {
+        Intent intent = new Intent(context, CustomToolbarAty.class);
+        context.startActivity(intent);
+    }
 }
--- ViewDemo/app/src/main/java/com/jeongho/viewdemo/toolbar/customToolbar.java
+++ ViewDemo/app/src/main/java/com/jeongho/viewdemo/toolbar/customToolbar.java
@@ -2,23 +2,68 @@ package com.jeongho.viewdemo.toolbar;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.jeongho.viewdemo.R;
 
 /**
  * Created by Jeongho on 2016/9/9.
  */
-public class customToolbar extends Toolbar{
-    public customToolbar(Context context) {
+public class CustomToolbar extends Toolbar{
+
+    private LayoutInflater mLayoutInflater;
+    private TextView mCenterTv;
+    private ImageButton mRightIb;
+    public CustomToolbar(Context context) {
         this(context, null);
     }
 
-    public customToolbar(Context context, @Nullable AttributeSet attrs) {
+    public CustomToolbar(Context context, @Nullable AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public customToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public CustomToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        initCenterView();
+    }
+
+    private void initCenterView() {
+        mLayoutInflater = LayoutInflater.from(getContext());
+        View centerView = mLayoutInflater.inflate(R.layout.custom_toolbar, null);
+        mCenterTv = (TextView) centerView.findViewById(R.id.tv_center);
+        mRightIb = (ImageButton) centerView.findViewById(R.id.ib_right);
+
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER_HORIZONTAL);
+        addView(centerView, layoutParams);
+    }
+
+    @Override
+    public void setTitle(@StringRes int resId) {
+        setTitle(getContext().getString(resId));
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+
+        if (mLayoutInflater == null){
+            initCenterView();
+        }
+        if (mCenterTv != null){
+            mCenterTv.setText(title);
+        }
+    }
+
+    public void setRightOnClickListener(OnClickListener listener){
+        mRightIb.setOnClickListener(listener);
     }
 }
